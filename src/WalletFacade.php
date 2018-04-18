3c9faa4881b953a17ac70da1b3f49778f49f4646
aveeto <35963794+aveeto@users.noreply.github.com>
2018-04-17 21:54:36 -0400

    Update WalletFacade.php

--- src/WalletFacade.php
+++ src/WalletFacade.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Depsimon\Wallet;
+namespace Aveeto\Wallet;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Depsimon\Wallet\Wallet
+ * @see \Aveeto\Wallet\Wallet
  */
 class WalletFacade extends Facade
 {
